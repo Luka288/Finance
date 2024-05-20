1662d9ea00b167a4b8812a53031382c53d92c18f
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-05-21 01:57:56 +0400

    Static display on main page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { CryptoRatesService } from '../../shared/services/crypto-rates.service';
 import { CryptoResponse } from '../../shared/interfaces/cryptoInter';
@@ -10,12 +10,16 @@ import { CryptoResponse } from '../../shared/interfaces/cryptoInter';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export default class HomeComponent {
+export default class HomeComponent implements OnInit {
     private readonly cryptoRequest = inject(CryptoRatesService)
 
     readonly cryptoArray: CryptoResponse[] = [];
 
     cryptoData: CryptoResponse  | null = null;
+
+ngOnInit(): void {
+  this.getCrypto();
+}
 
     getCrypto(){
       return this.cryptoRequest.getCrypto().subscribe((res) => {
--- src/app/shared/interfaces/cryptoInter.ts
+++ src/app/shared/interfaces/cryptoInter.ts
@@ -15,4 +15,3 @@ export interface CryptoResponse {
     }[];
     timestamp: number;
   }
-  
