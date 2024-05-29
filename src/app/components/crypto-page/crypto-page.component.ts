73635f85a3dae2759c8df15a528d4db03d0e4537
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-05-29 18:36:03 +0400

    working on updates

--- src/app/components/crypto-page/crypto-page.component.ts
+++ src/app/components/crypto-page/crypto-page.component.ts
@@ -3,6 +3,8 @@ import { CryptoRatesService } from '../../shared/services/crypto-rates.service';
 import { RouterModule } from '@angular/router';
 import { CryptoResponse } from '../../shared/interfaces/cryptoInter';
 import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs/internal/observable/interval';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-crypto-page',
@@ -15,17 +17,23 @@ export default class CryptoPageComponent implements OnInit{
   private readonly request = inject(CryptoRatesService)
 
   crypto: CryptoResponse[] = [];
-  limit: number = 15;
-  offset: number = 0;  
+  limit: number = 20;
+  offset: number = 0; 
 
   ngOnInit(): void {
+  this.loadCrypto();
+  const interval$ = interval(10000); // 60000 milliseconds = 1 minute
+
+  interval$.pipe(
+    take(1)
+  ).subscribe(() => {
     this.loadCrypto();
+  });
   }
 
   loadCrypto(){
     this.request.getCrypto(this.limit, this.offset).subscribe((res) => {
       this.crypto.push(res)
-      // this.crypto = this.crypto.concat(res);
       this.offset += this.limit;
       console.log(this.crypto)
     })
